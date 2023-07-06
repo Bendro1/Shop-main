bec47af45820cf6de2bf6a4ac37b591dbbc7b633
Bendro1 <jozef.1bednar@gmail.com>
2023-07-06 19:28:04 +0200

    I make test

--- src/test/java/com/learn/Shop/ShopApplicationTests.java
+++ src/test/java/com/learn/Shop/ShopApplicationTests.java
@@ -1,13 +1,23 @@
 package com.learn.Shop;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
+
+
 
 @SpringBootTest
 class ShopApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void test(){
+		int a =2;
+		int b =3;
+		Assert.assertEquals(a+b,5);
 	}
+
+
 
 }
